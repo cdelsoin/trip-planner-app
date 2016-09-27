b3643f628dadef111deae5f85872abd3674a611b
cdelsoin <cdelsoin@gmail.com>
2016-09-27 16:31:44 -0400

    add trips route
    user can view trips

--- app/router.js
+++ app/router.js
@@ -10,6 +10,9 @@ Router.map(function () {
   this.route('sign-in');
   this.route('change-password');
   this.route('users');
+  this.route('trips');
+  this.route('trip', { path: 'trips/:trip_id' });
+  this.route('plans');
 });
 
 export default Router;
